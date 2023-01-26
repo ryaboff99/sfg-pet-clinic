e0925b18ead44c180917c18b1ea8f7ee48c839be
Oleksii Riabov <ryaboff99@gmail.com>
2023-01-26 21:57:39 +0200

    Update OwnerControllerTest
    * Update method names of OwnerControllerTest
    * Add DisplayName annotations to methods of OwnerControllerTest

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -3,19 +3,17 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.ui.Model;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -23,8 +21,8 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-// TODO: write tests for all controllers and Services
 @ExtendWith(MockitoExtension.class)
+@DisplayName("Owner Controller Test")
 class OwnerControllerTest {
 
     @Mock
@@ -33,22 +31,21 @@ class OwnerControllerTest {
     @InjectMocks
     OwnerController ownerController;
 
-    Set<Owner> owners;
+    Set<Owner> ownerSet;
 
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        owners = new HashSet<>();
-        owners.add(Owner.builder().id(1L).build());
-        owners.add(Owner.builder().id(2L).build());
+        ownerSet = Set.of(Owner.builder().id(1L).build(), Owner.builder().id(2L).build());
 
         mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
     }
 
     @Test
-    void listOwners() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
+    @DisplayName("Controller list Owners")
+    void controllerListOwners() throws Exception {
+        when(ownerService.findAll()).thenReturn(ownerSet);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/owners"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -57,8 +54,9 @@ class OwnerControllerTest {
     }
 
     @Test
-    void listOwnersByIndex() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
+    @DisplayName("Controller list Ownerse by index")
+    void ControllerlistOwnersByIndex() throws Exception {
+        when(ownerService.findAll()).thenReturn(ownerSet);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/index"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -67,8 +65,9 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller list Owners by index.html")
     void listOwnersByIndexHtml() throws Exception {
-        when(ownerService.findAll()).thenReturn(owners);
+        when(ownerService.findAll()).thenReturn(ownerSet);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/index.html"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -77,7 +76,8 @@ class OwnerControllerTest {
     }
 
     @Test
-    void findOwners() throws Exception {
+    @DisplayName("Controller find Owners")
+    void controllerFindOwners() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("notimplemented"));
