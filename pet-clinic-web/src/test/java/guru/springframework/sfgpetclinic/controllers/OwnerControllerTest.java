36a4f65dc7d0a1dcf6cf4c0b1504878ff2d791f0
raw <ryaboff99@gmail.com>
2023-03-16 01:35:30 +0200

    Added test for empty find owner by last name to return all Owners. Closes #73
    * Add processFindFromEmptyReturnMany() method to OwnerControllerTest

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -48,7 +48,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    @DisplayName("Controller find Owners")
+    @DisplayName("Controller opens 'find owners' page to find Owners by Last name")
     void controllerFindOwners() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/find"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -58,6 +58,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller finds multiple Owners on 'find owners' page by Last name and displays them")
     void processFindFromReturnMany()  throws  Exception {
         when(ownerService.findAllByLastNameLike(anyString())).
                 thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
@@ -70,6 +71,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller finds single Owner on 'find owners' page by Last name and displays it")
     void processFindFormReturnOne() throws Exception {
         when(ownerService.findAllByLastNameLike(anyString())).
                 thenReturn(Arrays.asList(Owner.builder().id(1L).build()));
@@ -80,6 +82,22 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller do not find Owner by Last name on 'find owners' page and display all Owners")
+    void processFindFromEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
+                        Owner.builder().id(2L).build()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+//                        .param("lastName", "")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
+    @DisplayName("Controller display Owner information by Owner ID value")
     void displayOwner() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
 
@@ -90,6 +108,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller opens form to create new Owner")
     void initCreatingForm() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/owners/new"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -100,6 +119,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller takes new Owner from form, saves it to DB and displays")
     void processCreationForm() throws Exception {
         when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
 
@@ -112,6 +132,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller opens form to update Owner")
     void initUpdateOwnerForm() throws Exception {
         when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
 
@@ -122,6 +143,7 @@ class OwnerControllerTest {
     }
 
     @Test
+    @DisplayName("Controller takes updated Owner from form, saves it to DB and displays")
     void processUpdateOwnerForm() throws Exception {
         when(ownerService.save(ArgumentMatchers.any())).thenReturn(Owner.builder().id(1L).build());
 
